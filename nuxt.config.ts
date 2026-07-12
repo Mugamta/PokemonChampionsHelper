a5866fc3974417276e53e2631c197afb04f29e22
Mugamta <tjwlgns02@gmail.com>
2026-07-12 14:31:25 +0900

    github 페이지 오류 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 export default defineNuxtConfig({
 	modules: ['vuetify-nuxt-module'],
 
+	ssr: false,   // ← 추가: 실시간 시계 등 클라이언트 전용 데이터의 하이드레이션 불일치 방지
+
 	vuetify: {
 		vuetifyOptions: {
 			theme: {
